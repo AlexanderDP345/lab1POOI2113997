00eca5a9f05b52f89042aa78ded9d2f5bd1edba2
AlexanderDP345 <dpkevin835@gmail.com>
2023-09-02 00:41:43 -0500

    Primer Commit

---
+++ src/tareasem3/TareaSem3.java
@@ -0,0 +1,32 @@
+
+package tareasem3;
+
+public class TareaSem3 {
+
+    public static void main(String[] args) {
+        if (6<6*5)
+            System.out.print("hello");
+            System.out.print("There");
+            
+            //Resultado Hello There//
+            
+      int x=1, y=2, z=3;
+if(x>y)
+if(x>z)
+System.out.println("1111");
+else
+System.out.println("2222");      
+//Resultado 2222, el codigo no lo imprime porque el 2222 no esta asociado con el primer if, sino el 1111 lo esta, pero como este no es el caso el 2222 no se imprime//
+
+
+if (x < z)
+    System.out.println("*"); 
+else if (x == 2) 
+    System.out.println("&");
+else
+    System.out.println("$");
+
+// Resultado (*) //
+    }
+    
+}
